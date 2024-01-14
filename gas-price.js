28546e4a14c7b9a4216b30a9f48c6b20b9c4abe3
OutsideOne <125737107+OutsideOne@users.noreply.github.com>
2024-01-14 10:26:27 +0800

    gas-price.js

--- gas-price.js
+++ gas-price.js
@@ -1,22 +1,8 @@
 // 汽油价格查询解析(Stash脚本)
 //
-// @author RS0485
-// @repo [https://github.com/RS0485/network-rules](https://github.com/RS0485/network-rules)
-// @version 1.0.5
-//
-// Change Logs:
-//   - v1.0.3 支持油价调整趋势提示
-//   - v1.0.4 修复油价趋势解析
-//   - v1.0.5 默认查询成都油价，并将查询地区保存到PersistentStore
-//
 
 // 指定查询地区，可通过argument或persistentStore设置，后者优先级高
-var region = 'chengdu';
-
-const region_pref = <span class="math-inline">persistentStore\.read\("gas\_price\_region"\);
-if \(typeof region\_pref \!\=\= 'undefined' && region\_pref \!\=\= ''\) \{
-region \= region\_pref;
-\}
+var region = <span class="math-inline">persistentStore\.read\("gas\_price\_region", "chengdu"\);
 const query\_addr \= \`http\://m\.qiyoujiage\.com/</span>{region}.shtml`;
 
 <span class="math-inline">httpClient\.get\(
@@ -45,26 +31,24 @@ console\.log\(\`解析油价信息失败, 请反馈至 @RS0485\: URL\=</span>{qu
 
                 prices.push({
                     name: m[1],
-                    value: `<span class="math-inline">\{m\[2\]\} 元/L\`
-\}\);
-\}
-// 解析油价调整趋势
-var adjust\_date \= '';
-var adjust\_trend \= '';
-var adjust\_value \= '';
-const reg\_adjust\_tips \= /<div class\="tishi"\> <span\>\(\.\*\)<\\/span\><br\\/\>\(\[\\s\\S\]\+?\)<br\\/\>/;
-const adjust\_tips\_match \= data\.match\(reg\_adjust\_tips\);
-if \(adjust\_tips\_match && adjust\_tips\_match\.length \=\=\= 3\) \{
-adjust\_date \= adjust\_tips\_match\[1\]\.split\('价'\)\[1\]\.slice\(0, \-2\);
-adjust\_value \= adjust\_tips\_match\[2\];
-adjust\_trend \= \(adjust\_value\.indexOf\('下调'\) \> \-1 \|\| adjust\_value\.indexOf\('下跌'\) \> \-1\) ? '↓' \: '↑';
-const adjust\_value\_re \= /\(\[\\d\\\.\]\+\)元\\/升\-\(\[\\d\\\.\]\+\)元\\/升/;
-const adjust\_value\_re2 \= /\[\\d\\\.\]\+元\\/吨/;
-const adjust\_value\_match \= adjust\_value\.match\(adjust\_value\_re\);
-if \(adjust\_value\_match && adjust\_value\_match\.length \=\=\= 3\) \{
-adjust\_value \= \`</span>{adjust_value_match[1]}-${adjust_value_match[2]}元/L`;
-                }
-                else {
-                    const adjust_value_match2 = adjust_value.match(adjust_value_re2);
+                    value: `${m[2]} 元/L`
+                });
+            }
 
-   
+            // 解析油价调整趋势
+            var adjust_date = '';
+            var adjust_trend = '';
+            var adjust_value = '';
+
+            const reg_adjust_tips = /<div class="tishi"> <span>(.*)<\/span><br\/>([\s\S]+?)<br\/>/;
+            const adjust_tips_match = data.match(reg_adjust_tips);
+
+            if (adjust_tips_match && adjust_tips_match.length === 3) {
+                adjust_date = adjust_tips_match[1].split('价')[1].slice(0, -2);
+
+                adjust_value = adjust_tips_match[2];
+                adjust_trend = (adjust_value.indexOf('下调') > -1 || adjust_value.indexOf('下跌') > -1) ? '↓' : '↑';
+
+                const adjust_value_re = /([\d\.]+)元\/升-([\d\.]+)元\/升/;
+                const adjust_value_re2 = /[\d\.]+元\/吨/;
+                const adjust_value_match = adjust_value
