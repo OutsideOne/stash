ac14f6e7fe11a6c25b03ffe9d72f801239c560c3
OutsideOne <125737107+OutsideOne@users.noreply.github.com>
2024-01-14 12:31:32 +0800

    gas-price.js

--- gas-price.js
+++ gas-price.js
@@ -1,63 +1,35 @@
-const region = "成都";
+// 导入所需的库
+import axios from "axios";
 
-const query_addr = `http://m.qiyoujiage.com/${region}.shtml`;
+// 定义请求 URL
+const url = "http://m.qiyoujiage.com/sichuan/chengdu.shtml";
 
-const xhr = new XMLHttpRequest();
-xhr.open("GET", query_addr);
-xhr.responseType = "text";
-xhr.onload = function() {
-  if (xhr.status === 200) {
-    const data = xhr.responseText;
+// 发送请求
+axios.get(url).then((response) => {
+  // 检查响应是否成功
+  if (response.status === 200) {
+    // 解析响应数据
+    const data = response.data;
 
-    const reg_price = /<dl>[\s\S]+?<dt>(.*油)<\/dt>[\s\S]+?<dd>(.*)\(元\/升\)<\/dd>/gm;
+    // 获取油价信息
+    const oilPrices = data.data.list;
 
-    const prices = [];
-    let m;
+    // 过滤油价信息
+    const filteredOilPrices = oilPrices.filter((oilPrice) => {
+      return oilPrice.oil === "92#汽油";
+    });
 
-    while ((m = reg_price.exec(data)) !== null) {
-      prices.push({
-        name: m[1],
-        value: `${m[2]} 元/升`
-      });
-    }
-
-    // 解析油价调整趋势
-    const adjust_date = "";
-    const adjust_trend = "";
-    const adjust_value = "";
-
-    const reg_adjust_tips = /<div class="tishi"> <span>(.*)<\/span><br\/>([\s\S]+?)<\/dd>/;
-    const adjust_tips_match = data.match(reg_adjust_tips);
-
-    if (adjust_tips_match && adjust_tips_match.length === 3) {
-      adjust_date = adjust_tips_match[1].split("价")[1].slice(0, -2);
-
-      adjust_value = adjust_tips_match[2];
-      adjust_trend = (adjust_value.indexOf("下调") > -1 || adjust_value.indexOf("下跌") > -1) ? "↓" : "↑";
-
-      // 提取调整幅度
-      const adjust_value_re = /([\d\.]+)元\/升-([\d\.]+)元\/升/;
-      const adjust_value_re2 = /[\d\.]+元\/升/; // 仅匹配单一升价
-      const adjust_value_match = adjust_value.match(adjust_value_re);
-
-      if (adjust_value_match && adjust_value_match.length === 3) {
-        adjust_value = `${adjust_value_match[1]}-${adjust_value_match[2]} 元/升`;
-      } else {
-        adjust_value_match2 = adjust_value.match(adjust_value_re2);
-        if (adjust_value_match2) {
-          adjust_value = adjust_value_match2[0];
-        }
-      }
-    }
-
-    done({
-      prices,
-      adjust_date,
-      adjust_trend,
-      adjust_value,
+    // 遍历油价信息
+    filteredOilPrices.forEach((oilPrice) => {
+      // 打印油价信息
+      console.log(`
+          油品：${oilPrice.oil}
+          价格：${oilPrice.price}
+          单位：${oilPrice.unit}
+        `);
     });
   } else {
-    done({});
+    // 响应失败
+    console.log("请求失败");
   }
-};
-xhr.send();
+});
